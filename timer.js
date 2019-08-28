cae1bbf2ac3464167f241c35264860440fbaf4dc
Jimmy <jdicolandrea@gmail.com>
2019-08-27 20:39:28 -0400

    correct date logged

--- timer.js
+++ timer.js
@@ -1,5 +1,13 @@
 function timer() {
-  console.log(Date.now())
+  let endDate = new Date('June 12, 2020')
+  let currentDate = new Date()
+  console.log(endDate, currentDate)
+  let diff = Math.ceil((endDate - currentDate)/3600000/24)
+  // let diffSecs = diff/1000 
+  // let diffMins = diffSecs/60 
+  // let diffHours = diffMins/60 
+  // let diffDays = diffHours/24
+  console.log(diff)
 }
 
 timer()
